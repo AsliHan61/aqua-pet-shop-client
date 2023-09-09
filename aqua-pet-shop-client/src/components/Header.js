86a07c96fe7a71347f3aec330ee9e2c34f7b20a3
AsliHan61 <asli.61.y@gmail.com>
2023-09-09 14:58:01 +0200

    Gitwhat

---
+++ aqua-pet-shop-client/src/components/Header.js
@@ -0,0 +1,15 @@
+
+function Header() {
+    return (
+
+<header> 
+    <div>
+        <div>
+          <img src="../assets/img/header.png" alt="Image Header" />
+        </div>
+    </div>
+</header>
+    )
+    };
+
+    export default Header;
